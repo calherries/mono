d30b3171564082810123b9aab3dc87a37a8c6205
Callum Herries <hi@callumherries.com>
2021-10-14 21:23:42 +0100

    Add a server with a basic request handler

--- frontend-practice/ableton/src/ableton/core.clj
+++ frontend-practice/ableton/src/ableton/core.clj
@@ -1,1 +1,28 @@
-(ns ableton.core)
+(ns ableton.core
+  (:require [ring.adapter.jetty :as ring-jetty]))
+
+;; Globals
+(defonce server (atom nil))
+
+(defn handler [req]
+  {:status 200
+   :body "hello, world"})
+
+(defn start-server []
+  (reset! server (ring-jetty/run-jetty #'handler {:join? false :port 8001})))
+
+(defn stop-server []
+  (.stop @server)
+  (reset! server nil))
+
+(defn reset-server []
+  (stop-server)
+  (start-server))
+
+;; Init
+(start-server)
+
+(comment
+  (reset-server)
+  (keys (ns-publics 'ring.adapter.jetty))
+  )
