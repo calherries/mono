edf1a577ce69edd4bf460301264c6744e3d3f699
Callum Herries <hi@callumherries.com>
2021-10-16 08:17:39 +0100

    Add main component with heading and subheading

--- frontend-practice/ableton/src/ableton/layout.clj
+++ frontend-practice/ableton/src/ableton/layout.clj
@@ -37,6 +37,30 @@
      [:a {:href ""}
       "HOME"]]]])
 
+(defn main []
+  [:main
+   [:div (use-style
+          {:display        :flex
+           :flex-direction :column
+           :align-items   :center
+           :gap            "10px"
+           :padding        "30 20 30 20"
+           :background-color "#F9EBD7"})
+    [:h1 (use-style
+          {:margin      0
+           :text-align  :center
+           :line-height 1.1
+           :font-size   "60px"
+           :font-family "DM Serif Display"})
+     "Become a better frontend developer"]
+    [:h2 (use-style
+          {:text-align  :center
+           :font-weight :normal})
+     "Take your skills to the next level by recreating "
+     [:span (use-style
+             {:font-weight :bold})
+      "real websites"]]]])
+
 (defn page []
   [:html
    [:head
@@ -48,5 +72,7 @@
    [:body (use-style
            {:line-height 1.5
             :margin      0
-            :font-family "Archivo,sans-serif"})
-    (header)]])
+            :font-family "Archivo,sans-serif"
+            :color       "rgba(0,0,0,.8)"})
+    (header)
+    (main)]])
