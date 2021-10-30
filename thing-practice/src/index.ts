27b0dcdc7f5e2e687d39a4ce0eac80c772ed61f7
Callum Herries <hi@callumherries.com>
2021-10-30 15:59:22 +0200

    Site working

--- thing-practice/src/index.ts
+++ thing-practice/src/index.ts
@@ -1,3 +1,8 @@
-import { $compile } from "@thi.ng/rdom"
+import {} from "@thi.ng/rstream";
+import { $compile } from "@thi.ng/rdom";
+import {} from "@thi.ng/parse";
+import {} from "@thi.ng/transducers";
 
-$compile(["div", {}, "hello"]).mount(document.getElementById("app")!);
+$compile(
+    ["div", {}, ["h1", {}, "Hello, Youtube"]]
+).mount(document.body)
