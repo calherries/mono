16d001775d17bc9d8b0d6e9bfb040f12a3b1e6ea
Callum Herries <hi@callumherries.com>
2021-10-30 15:00:49 +0200

    Ex 1

--- datalevin-practice/sql_exercises.clj
+++ datalevin-practice/sql_exercises.clj
@@ -19,6 +19,7 @@
 
 ;; Warm-up
 (d/q '[:find ?salesman-id ?name ?city
+       :keys salesman-id name city
        :where
        [_ :name ?name]
        [_ :city ?city]
@@ -26,3 +27,42 @@
   (d/db conn)) ; db is a reference to a database
 
 ;; 1. Write a SQL statement to display all the information of all salesmen
+
+(d/q '[:find (pull ?e [*])
+       :where
+       [?e _ _]]
+  (d/db conn))
+;; => ([{:city "Paris",
+;;       :commission 0.14,
+;;       :db/id 4,
+;;       :name "Mc Lyon",
+;;       :salesman-id 5006}]
+;;     [{:city "San Jose",
+;;       :commission 0.12,
+;;       :db/id 6,
+;;       :name "Lauson Hen",
+;;       :salesman-id 5003}]
+;;     [{:city "London",
+;;       :commission 0.11,
+;;       :db/id 3,
+;;       :name "Pit Alex",
+;;       :salesman-id 5005}]
+;;     [{:city "Rome",
+;;       :commission 0.13,
+;;       :db/id 5,
+;;       :name "Paul Adam",
+;;       :salesman-id 5007}]
+;;     [{:city "Paris",
+;;       :commission 0.13,
+;;       :db/id 2,
+;;       :name "Nail Knite",
+;;       :salesman-id 5002}]
+;;     [{:city "New York",
+;;       :commission 0.15,
+;;       :db/id 1,
+;;       :name "James Hoog",
+;;       :salesman-id 5001}])
+
+;; 2. Write a SQL statement to display a string "This is SQL Exercise, Practice and Solution".
+
+; Not practical in datalog
