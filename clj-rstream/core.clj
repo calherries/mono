422e7bda0bc8be8dfed949a64d5a433cffb8fc82
Callum Herries <hi@callumherries.com>
2021-10-31 15:20:48 +0100

    Add stream record and constructor

--- clj-rstream/core.clj
+++ clj-rstream/core.clj
@@ -10,21 +10,46 @@
   (unsubscribe-self [this])
   (unsubscribe-child [this subscriber]))
 
+(defprotocol ISubscription
+  (state [this])
+  (parent [this]))
+
 (defrecord Trace []
   ISubscriber
   (just [trace x] (prn x))
   (done [trace] (prn "done"))
   (error [trace e] (prn "error" e)))
 
-(comment
-  (doto (->Trace)
-    (just "p")
-    (done)
-    (error "p"))
-  )
+; WIP
+(defrecord Stream [src state parent subs]
+  ISubscribable
+  (subscribe [stream subscriber] 1)
+  (unsubscribe-self [stream] 1)
+  (unsubscribe-child [stream subscriber] 1)
+  ISubscriber
+  (just [stream x] (prn x))
+  (done [stream] (prn "done"))
+  (error [stream e] (prn "error" e))
+  ISubscription
+  (state [stream] (:state stream))
+  (parent [stream] (:parent stream)))
+
+(defn make-stream [src]
+  (->Stream src :idle nil []))
 
 (comment
-  (def a (stream (fn (s)
+
+  (let [stream (make-stream (fn [s] "hey"))]
+    (state stream))
+
+  (let [src (fn [s]
+              (doto s
+                (just "p")
+                (error "p")
+                (done)))]))
+
+(comment
+  (def a (stream (fn [s]
                    (doto s
                      (just 1)
                      (just 2)
