c58a5a3c9057b1e612681ca74b5fbe3f1a1e248d
Callum Herries <hi@callumherries.com>
2021-12-07 21:56:29 +0300

    Day 2

--- aoc/2021/day_2.clj
+++ aoc/2021/day_2.clj
@@ -8,11 +8,22 @@
               (let [[x y] (str/split line #"\s")]
                 [(keyword x) (read-string y)])))))
 
+;; Part 1
 (let [[x-pos y-pos] (reduce (fn [[x-pos y-pos] [command distance]]
                               (case command
                                 :forward [(+ x-pos distance) y-pos]
                                 :down    [x-pos (+ y-pos distance)]
                                 :up      [x-pos (- y-pos distance)]))
                             [0 0]
+                            input)]
+  (* x-pos y-pos))
+
+;; Part 2
+(let [[x-pos y-pos aim] (reduce (fn [[x-pos y-pos aim] [command distance]]
+                              (case command
+                                :forward [(+ x-pos distance) (+ (* aim distance) y-pos) aim]
+                                :down    [x-pos y-pos (+ aim distance)]
+                                :up      [x-pos y-pos (- aim distance)]))
+                            [0 0 0]
                             input)]
     (* x-pos y-pos))
