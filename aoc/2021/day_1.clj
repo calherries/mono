6c28f2ceeb50b98b25cc283f75fa9cf2b6804cbb
Callum Herries <hi@callumherries.com>
2021-12-07 21:45:17 +0300

    Day 1

--- aoc/2021/day_1.clj
+++ aoc/2021/day_1.clj
@@ -6,8 +6,12 @@
        (str/split-lines)
        (map read-string)))
 
+;; Part 1
 (->> input
      (partition 2 1)
      (map #(apply < %))
      (filter true?)
      (count))
+
+;; Part 2
+(count (filter true? (map < input (drop 3 input))))
